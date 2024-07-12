ade242bf737f878e3c1f7f1afe16ad03aa68b554
alexandraazzena <168538339+alexandraazzena@users.noreply.github.com>
2024-07-12 16:39:36 +0200

    Update Ingredient_main.py

--- Ingredient_main.py
+++ Ingredient_main.py
@@ -6,7 +6,7 @@ from functions import scarica_pdf, estrai_testo_pdf, estrai_contesto, controllo_
 
 
 def main():
-    st.title("SafeCosmetic")
+    st.title(":biohazard_sign: SafeCosmetic :biohazard_sign:")
 
     with open('Cir.html', encoding='utf-8') as fp1:
         soup = BeautifulSoup(fp1, "html.parser")
